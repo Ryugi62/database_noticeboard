9a99cd774e183194166f122b9f84a58642a37b21
Ryugi62 <xorjf1027@naver.com>
2024-11-26 11:09:10 +0900

    feat : Footer 디자인 구현.

--- Components/FooterComponent.php
+++ Components/FooterComponent.php
@@ -1,6 +1,16 @@
 <footer id="footer-component" class="view-box">
     <div class="footer-box view">
-        <h1>This is Footer Components</h1>
+
+        <img src="/assets/img/simbol.png" alt="" srcset="" class="simbol">
+
+        <hr>
+
+        <p><strong>상호명:</strong> 데이터베이스 과제</p>
+        <p><strong>제출자:</strong> 김태걸</p>
+        <p><strong>주소:</strong> 경남 창원시 의창구 용동 186 98호관 1층</p>
+        <p><strong>학번:</strong> 20223052</p>
+        <p><strong>개인정보관리책임자:</strong> 김태걸</p>
+        <p><strong>전화:</strong> 010-1234-1234 <strong>팩스:</strong> 010-1234-1234 <strong>이메일:</strong> <a href="mailto:xorjf1027@naver.com">xorjf1027@naver.com</a></p>
     </div>
 </footer>
 
@@ -10,8 +20,18 @@
 
 <style>
     #footer-component {
+        text-align: center;
+
         * {
             /* border: 1px solid red; */
+        }
+
+        .simbol {
+            height: 50px;
+            display: flex;
+            padding: 30px 0 15px 0;
+            align-items: center;
+            justify-content: left;
         }
     }
 </style>
