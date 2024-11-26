7671e719e9cd7006dc1e7265cef212abc3762bfc
Ryugi62 <xorjf1027@naver.com>
2024-11-26 14:08:19 +0900

    feat : 게시판 상세페이지 클래스 및 html 구조 수정.

--- PostDetail.php
+++ PostDetail.php
@@ -12,8 +12,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
 
-    <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
-
     <?php
     require_once './config/database.php';
     ?>
@@ -27,7 +25,19 @@
 
     <main>
         <div class="main-component view">
-            <h1>Hello World</h1>
+            <div class="page-header">
+                <strong class="page-title">게시판 상세페이지</strong>
+            </div>
+
+            <div class="post-detail-continaer">
+                <h2 class="post-title">대충 제목</h2>
+
+
+            </div>
+
+
+
+            <a href="/"><button class="go-post-list-button">글 목록</button></a>
         </div>
     </main>
 
@@ -37,17 +47,71 @@
 </body>
 
 <script>
-    // CKEditor 초기화
-    ClassicEditor
-        .create(document.querySelector('#editor'))
-        .catch(error => {
-            console.error(error);
-        });
+
 </script>
 
-
 <style>
-    .main-component {}
+    .main-component {
+        margin: 50px 0;
+        height: 100%;
+
+        * {
+            /* border: 1px solid red; */
+        }
+
+        .post-detail-continaer {
+            min-height: 580px;
+        }
+
+        .post-title {
+            margin: 0;
+            padding: 16px 0;
+        }
+
+        .page-header {
+            height: 31px;
+            display: flex;
+            align-items: center;
+            border-bottom: 1px solid black;
+            padding-bottom: 16px;
+            justify-content: space-between;
+        }
+
+        .page-title {
+            font-size: 18px;
+        }
+
+        table {
+            width: 100%;
+            text-align: center;
+            border-top: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 10px 10px;
+        }
+
+        tr {
+            border-bottom: 1px solid #e5e7eb;
+        }
+
+        tbody tr:hover {
+            background-color: #4f4f4f0a;
+        }
+
+        .post-title {
+            text-align: left;
+        }
+
+        .go-post-list-button {
+            color: white;
+            margin: auto;
+            display: flex;
+            background-color: #2a2a2a;
+        }
+    }
 </style>
 
 </html>
